c0474576369c018ad5b4ddaaccabec18b7036429
Can Akcan <c.akcan96@gmail.com>
2021-01-20 21:37:31 +0300

    Proje

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyC4pPUeAZ5F9LXv5Tv2wWmr5R7rWtXDZUE",
+    authDomain: "ionic-login-c5a4a.firebaseapp.com",
+    projectId: "ionic-login-c5a4a",
+    storageBucket: "ionic-login-c5a4a.appspot.com",
+    messagingSenderId: "813973865130",
+    appId: "1:813973865130:web:9d9430fa5c7bc10c4b06d3"
+  };
